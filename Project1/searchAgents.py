9be7a0033fe0b43ea33ebdf990b8fe43204f816d
Khris Thammavong <khristha@gmail.com>
2022-02-02 20:55:33 -0700

    Working on defining corners search problem

--- Project1/searchAgents.py
+++ Project1/searchAgents.py
@@ -287,22 +287,43 @@ class CornersProblem(search.SearchProblem):
         self._expanded = 0 # DO NOT CHANGE; Number of search nodes expanded
         # Please add any code here which you would like to use
         # in initializing the problem
-        "*** YOUR CODE HERE ***"
+
+        # Represents if each of the respective corners in self.corners has been visited
+        self.cornersVisited = [False, False, False, False]
 
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # checking off cornersVisited too early? 
+        # if state == self.corners[0]:
+        #     self.cornersVisited[0] = True
+        # elif state == self.corners[1]:
+        #     self.cornersVisited[1] = True
+        # elif state == self.corners[2]:
+        #     self.cornersVisited[2] = True
+        # elif state == self.corners[3]:
+        #     self.cornersVisited[3] = True
+        
+            
+
+
+
+        for visitedCorner in self.cornersVisited:
+            if not visitedCorner:
+                # print("state: " + str(state) + " missing other corners: " + str(self.cornersVisited))
+                return False
+        
+        # print("have all corners " + str(self.cornersVisited))
+        return False
+
 
     def getSuccessors(self, state):
         """
@@ -319,12 +340,14 @@ class CornersProblem(search.SearchProblem):
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             # Add a successor state to the successor list if the action is legal
             # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
-
-            "*** YOUR CODE HERE ***"
+            x,y = state
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+            if not hitsWall:
+                nextState = (nextx, nexty)
+                cost = 1
+                successors.append( ( nextState, action, cost) )
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
